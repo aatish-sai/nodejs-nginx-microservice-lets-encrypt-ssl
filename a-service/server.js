c2c2bdaca34c40c57bf809c9fcea52f782550992
Aatish Sai <meatish.sai@gmail.com>
2022-12-24 02:36:31 +0545

    use node http

--- a-service/server.js
+++ a-service/server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const http = require("http");
 
 const app = express();
 
+app.get("/b", (_req, res) => {
+  http.get("http://b-service", (r) => {
+    let rawData = "";
+    r.on("data", (chunk) => {
+      rawData += chunk;
+    });
+    r.on("end", () => {
+      res.send(`Called service B from A ${rawData}`);
+    });
+  });
+});
 app.get("/", (_req, res) => {
   res.send("Hello from Service A");
-});
-
-app.get("/b", (_req, res) => {
-  fetch("http://b-service").then((response) => {
-    res.send(`Called service B from A ${response}`);
-  });
 });
 
 app.listen(80, () => {
