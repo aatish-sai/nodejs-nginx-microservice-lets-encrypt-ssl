8c9fa785f589d2a5d9ea54687a682430e50cb86a
Aatish Sai <meatish.sai@gmail.com>
2022-12-24 03:18:03 +0545

    eslint format

--- a-service/server.js
+++ a-service/server.js
@@ -1,23 +1,25 @@
 const express = require("express");
+
 const http = require("http");
 
 const app = express();
 
 app.get("/b", (_req, res) => {
-  http.get("http://b-service", (r) => {
-    let rawData = "";
-    r.on("data", (chunk) => {
-      rawData += chunk;
+    http.get("http://b-service", (r) => {
+        let rawData = "";
+        r.on("data", (chunk) => {
+            rawData += chunk;
+        });
+        r.on("end", () => {
+            res.send(`Called service B from A ${rawData}`);
+        });
     });
-    r.on("end", () => {
-      res.send(`Called service B from A ${rawData}`);
-    });
-  });
 });
+
 app.get("/", (_req, res) => {
-  res.send("Hello from Service A");
+    res.send("Hello from Service A");
 });
 
 app.listen(80, () => {
-  console.log("App running on port 80");
+    console.log("App running on port 80");
 });
--- b-service/server.js
+++ b-service/server.js
@@ -1,12 +1,11 @@
-
 const express = require("express");
 
 const app = express();
 
 app.get("/", (_req, res) => {
-    res.send("Hello from Service B")
-})
+    res.send("Hello from Service B");
+});
 
-app.listen(80, ()=> {
-    console.log("App running on port 80")
-})
+app.listen(80, () => {
+    console.log("App running on port 80");
+});
